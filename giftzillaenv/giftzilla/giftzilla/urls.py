5a65320daf49bd9269760a2b9e482295bc792694
Jeremy Kuehn <100542455+arkterra90@users.noreply.github.com>
2023-11-11 13:14:40 -0600

    updates 500 error handling

--- giftzillaenv/giftzilla/giftzilla/urls.py
+++ giftzillaenv/giftzilla/giftzilla/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import handler404
 
-from index.views import custom_404_view
+from index.views import custom_404_view, custom_500_view
 
 handler404 = custom_404_view
+
+handler500 = custom_500_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
